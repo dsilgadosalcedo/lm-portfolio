370cc6283754e818e957516eadbcb442712a6e0d
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-07-08 13:51:16 -0500

    Refactor Home page layout and button styles
    - Adjusted padding and margin for header and main sections for better spacing.
    - Updated hero section text sizes for improved responsiveness.
    - Enhanced experience section layout to support flexible wrapping.
    - Modified services section to include margin adjustments and responsive visibility for separators.
    - Updated button icon size for better consistency across different screen sizes.

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ import { Award } from "lucide-react";
 export default function Home() {
   return (
     <div className="grid min-h-screen font-[family-name:var(--font-sarala)]">
-      <main className="flex flex-col flex-1 items-center sm:items-start max-w-[1100px] w-full mx-auto px-7 overflow-hidden">
+      <main className="flex flex-col flex-1 items-center sm:items-start max-w-[1100px] w-full mx-auto md:px-7 overflow-hidden">
         {/* Header */}
-        <header className="flex items-center justify-between w-full my-6 px-20">
+        <header className="flex items-center justify-between w-full my-2 md:my-6 px-20">
           <h1 className="text-xl font-base text-primary">Linda Armesto</h1>
 
           <Tooltip>
@@ -35,17 +35,20 @@ export default function Home() {
           </Tooltip>
         </header>
 
-        <div className="flex-1 rounded-t-[3.5rem] border-t border-x border-border pt-14 px-20 w-full shadow-[0_-4px_20px_0_rgba(161,143,227,0.12)] relative">
+        <div className="flex-1 rounded-4xl md:rounded-t-[3.5rem] border-t border-x border-border md:pt-14 pt-8 px-4 md:px-20 w-full shadow-[0_-4px_20px_0_rgba(161,143,227,0.12)] relative">
           {/* Gradient */}
-          <div className="bg-gradient-to-b from-transparent to-background h-full w-screen absolute bottom-0 -left-30 z-10" />
+          <div className="bg-gradient-to-b from-transparent to-background h-full w-screen absolute bottom-0 md:-left-10 lg:-left-30 z-10" />
 
           {/* Hero */}
           <section className="flex justify-between relative z-20">
             <div>
-              <h2 className="text-[4rem] font-bold mb-0">
+              <h2 className="text-4xl md:text-[4rem] font-bold mb-2 md:mb-0">
                 Abogada <span className="text-primary">Tech</span>
               </h2>
-              <p className="text-xl mb-8 text-muted-foreground">
+              <p className="md:hidden text-lg mb-8 text-muted-foreground">
+                Asesoría y representación legal estratégica para el mundo tech
+              </p>
+              <p className="hidden md:block text-xl mb-8 text-muted-foreground">
                 Asesoría y representación legal estratégica
                 <br />
                 para el mundo tech
@@ -82,7 +85,7 @@ export default function Home() {
 
           {/* Experience */}
           <section className="relative z-20 py-8">
-            <ul className="flex gap-6">
+            <ul className="flex flex-wrap gap-6">
               <li className="flex items-center gap-2">
                 <div className="h-10 w-10 bg-border rounded-full relative overflow-hidden grid place-items-center">
                   <div className="h-6 w-6 bg-radial from-primary/50 to-border rounded-full absolute -bottom-2.5 left-1/2 -translate-x-1/2" />
@@ -114,7 +117,7 @@ export default function Home() {
           </section>
 
           {/* Services */}
-          <section className="bg-card rounded-[2.25rem] py-5 px-10 relative z-20">
+          <section className="bg-card mb-4 lg:mb-0 rounded-[2.25rem] py-5 px-10 relative z-20">
             <ul className="flex flex-wrap justify-center">
               {services.map((service, index) => (
                 <div key={`service-${index}`} className="flex items-center">
@@ -122,7 +125,7 @@ export default function Home() {
                     {service}
                   </li>
                   {index !== services.length - 1 && index !== 3 && (
-                    <div className="h-15">
+                    <div className="hidden lg:block h-15">
                       <Separator orientation="vertical" className="mx-4" />
                     </div>
                   )}
