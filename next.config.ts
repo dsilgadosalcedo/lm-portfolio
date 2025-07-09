5311612eb6562fcf23c30a67dfc264ebdbfa58c0
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-07-08 23:58:02 -0500

    Update Next.js configuration to disable image optimization
    - Set images configuration to unoptimized in next.config.ts for improved flexibility in image handling.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
+    unoptimized: true,
   },
 };
 
